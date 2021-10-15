688963ce58fbd623612a2d424f314c28f4c5f447
Emigdio Torres <emigdio821@gmail.com>
2021-10-15 04:26:00 -0500

    Added container and footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import Navbar from "components/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { BiCoffee } from "react-icons/bi";
 import "./App.scss";
-
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="App">
-        <Navbar />
+        <Navbar isDisabled />
+        <div className="container">Hi there...</div>
+        <footer>
+          <div className="footer-container">
+            <div className="footer-item">
+              <a
+                href="https://github.com/emigdio821"
+                target="_blank"
+                rel="noreferrer"
+                className="footer-link"
+              >
+                <span className="github-link">
+                  @emigdio821 <BiCoffee />
+                </span>
+              </a>
+            </div>
+            <div className="footer-item">
+              Ecommerce created during{" "}
+              <a
+                href="https://www.wizeline.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="footer-link wizeline"
+              >
+                <span>WIZE</span>
+                <span style={{ color: "#ff4d4f" }}>LINE</span>
+              </a>
+              ’s{" "}
+              <a
+                href="https://academy.wizeline.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="footer-link"
+              >
+                Academy
+              </a>{" "}
+              React<span style={{ fontWeight: "bold" }}>Bootcamp</span>
+            </div>
+          </div>
+        </footer>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
