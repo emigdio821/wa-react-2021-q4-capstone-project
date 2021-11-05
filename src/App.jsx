29558ddade30dc97648197ea4a3100aeee2df130
Emigdio Torres <emigdio821@gmail.com>
2021-11-05 09:50:48 -0600

    Refactored the routes code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import Navbar from 'components/Navbar';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom';
+import routes from 'Routes';
+import Navbar from 'components/Navbar';
 import Footer from 'components/Footer';
-import Home from 'pages/Home';
-import Products from 'pages/Products';
-import Product from 'pages/Product';
-import NotFound from 'pages/NotFound';
-import Search from 'pages/Search';
-import Cart from 'pages/Cart';
-import Checkout from 'pages/Checkout';
 import './App.scss';
 import ScrollTop from 'components/ScrollTop';
 
@@ -26,13 +20,15 @@ const App = () => (
           <Route exact path="/home">
             <Redirect to="/" />
           </Route>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/products" component={Products} />
-          <Route exact path="/product/:id" component={Product} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/cart" component={Cart} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route path="*" component={NotFound} />
+          {routes.map((route) => (
+            <Route
+              path={route.path}
+              exact={route.exact}
+              key={`${route.path}-route`}
+            >
+              <route.component />
+            </Route>
+          ))}
         </Switch>
       </div>
       <Footer />
